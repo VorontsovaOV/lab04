db9588d650704816e221e73ed224a0028d49d243
u211-05 <u211-05@public.mpei.local>
2017-05-11 17:07:46 +0300

    entering filepath

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 char nibble_to_hex(uint8_t byte)//перевод в 16-ричное число
 {
@@ -52,6 +53,16 @@ void print_in_hex(const void* data, size_t size)
     cout<<"  ";}
 }
 
+
+struct Student
+{char name[17];
+    uint16_t year;
+    float mid_mark;
+    int mf : 1;
+    int quantity;
+    Student* str;
+};
+
 int
 main() {
     uint8_t z = 200;
@@ -59,5 +70,46 @@ main() {
 
     int n = 42;
     print_in_hex(&n, sizeof(n));
+    cout<<'\n';
+
+    Student S[3];
+    strcpy(S[0].name,"Vasiliy");
+    S[0].year=2016;
+    S[0].mid_mark=4.2;
+    S[0].mf=1;
+    S[0].quantity=0;
+    S[0].str = nullptr;  // &S[0]
+
+    strcpy(S[1].name,"Uliya");
+    S[1].year = 2016;
+    S[1].mid_mark = 4.13;
+    S[1].mf=0;
+    S[1].quantity=0;
+    S[1].str = &S[0];
+
+    strcpy(S[2].name,"Vitaliy");
+    S[2].year=2016;
+    S[2].mid_mark=4.34;
+    S[2].mf=1;
+    S[2].quantity=0;
+    S[2].str = &S[0];
+
+    cout << &S << endl;
+    cout<<"Size of S - "<< sizeof S << '\n';
+
+cout<<"Mas. S:"<<'\n';
+    print_in_hex(&S, sizeof S); cout<<'\n';
+
+for(size_t i=0; i<3; i++){
+cout<<"Adress of el."<<i<<" - "<<&S[i]; cout<<"\nSize of el."<<i<<" - "<<sizeof
+            S[i]<<'\n';
+} cout<<'\n';
+
+  //  cout<<"El. mas. S #2";
+    char path[64]
+cout<<"Enter adress ";cin>>path;
+    bool path_d= true;
+    if (path[1]=':')
+                if ((strchr(path[0]))&&(path[2]=='\\'))
     return 0;
 }
